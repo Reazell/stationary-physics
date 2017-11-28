ec84e5ac1e828aed0a0ae3c019b4901788d07a06
Reazell <kapitankopytko@gmail.com>
2017-11-28 13:40:58 +0100

    Rope Swing Detach

---
+++ RopeSwing.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeSwing : MonoBehaviour {
+
+    public static HingeJoint2D hinge;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+
+
+        Debug.Log("BeforeCollision");
+        if (collider.gameObject.tag == "Player" && hinge == null)
+        {
+            Debug.Log("collsion");
+            hinge = gameObject.AddComponent<HingeJoint2D>();
+            hinge.connectedBody = collider.gameObject.GetComponent<Rigidbody2D>();
+            Invoke("Freedom" , 1);
+        }
+
+    }
+
+
+    void Freedom()
+    {
+        Destroy(hinge);
+        Debug.Log("FREEEDOOOM!");
+    }
+}
