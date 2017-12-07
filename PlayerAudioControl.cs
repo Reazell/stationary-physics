627d028f8911f9a90fb834f3956bb5e871efac1c
Reazell <kapitankopytko@gmail.com>
2017-12-07 09:44:04 +0100

    AudioFootstepController

---
+++ PlayerAudioControl.cs
@@ -0,0 +1,41 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class PlayerAudioControl : MonoBehaviour {
+
+    [SerializeField] AudioSource footstepsSource;
+    [SerializeField] AudioClip[] footsteps;
+
+    int lastFootstepIndex;
+
+    void Awake()
+    {
+
+    }
+
+    void PlayerRandomFootstep()
+    {
+        var index = 0;
+        while (index == lastFootstepIndex)
+        {
+            index = Random.Range(0, footsteps.Length);
+        }
+        lastFootstepIndex = index;
+        PlayClip(footsteps[index]);
+        Debug.Log("Playing footstep " + index);
+       
+    }
+
+
+    void PlayClip(AudioClip clip)
+    {
+        if (footstepsSource.isPlaying)
+            footstepsSource.Stop();
+            footstepsSource.clip = clip;
+            footstepsSource.Play();
+        
+    }
+}
