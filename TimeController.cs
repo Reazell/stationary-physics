a28d7b14a936a8cf03b3e410e41b0649ebc552aa
Reazell <kapitankopytko@gmail.com>
2017-12-12 13:15:41 +0100

    Add files via upload

---
+++ TimeController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeController : MonoBehaviour {
+
+
+    Rigidbody rigidbody;
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 Magnitude = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0f);
+        Time.timeScale = Magnitude.magnitude;
+        Time.fixedDeltaTime = 0.02f * Time.timeScale;
+    }
+}
