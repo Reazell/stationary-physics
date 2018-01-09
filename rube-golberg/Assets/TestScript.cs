b1d1cda021df8a3ce735e4c1a711bb070b6a3584
WSEI\szymon.przybyszewski <kapitankopytko@gmail.com>
2018-01-09 13:04:45 +0100

    Rube-Goldberg - In Progress

---
+++ rube-golberg/Assets/TestScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Time.timeScale = 10;
+	}
+
+    void OnCollisionEnter()
+    {
+        Time.timeScale = 1;
+    }
+}
