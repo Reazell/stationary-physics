f2cdf1e26b8b01cf385cd8c2c27ed527aa488d5e
Reazell <kapitankopytko@gmail.com>
2018-01-23 13:15:31 +0100

    JetPack simulation with particles

---
+++ class-scripts/jetpackScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jetpackScript : MonoBehaviour {
+
+    public ParticleSystem particles;
+    public Rigidbody rb;
+    [Range(1f, 100f)] public float speed;
+    
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            JetPackEnabled();
+        }
+        else
+        {
+            JetPackDisabled();
+        }
+
+        
+	}
+
+    public void JetPackEnabled()
+    {
+        particles.enableEmission = true;
+        //rb.AddForce(Vector3.up * speed, ForceMode.Acceleration);
+        rb.AddForce(transform.forward * speed, ForceMode.Acceleration);
+        rb.AddForce(transform.up * speed, ForceMode.Acceleration);
+    }
+
+    public void JetPackDisabled()
+    {
+        particles.enableEmission = false;
+    }
+}
